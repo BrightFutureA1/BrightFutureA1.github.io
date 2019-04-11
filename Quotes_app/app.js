6e602e7a0656a3447a986e81a1cdc3e162cb8025
brightfuturea1 <allasia.langston@gmail.com>
2019-04-11 09:53:08 -0400

    project_1

--- Quotes_app/app.js
+++ Quotes_app/app.js
@@ -4,18 +4,20 @@
 // let queryURL = baseURL + quoteType
 
 // console.log(queryURL)
-let topic =''
+// Global variable used to be defined anywhere my code so when I call the 
+// topic function
+let topic = ''
 
 const getTopic = () => {
-    topic = $('#input').text()
-    console.log(topic)
+    topic = $('#input').val()
+    console.log(topic);
 }
 
 const getQuotes = () => {
     $.ajax({
         type: "GET",
         dataType: "json",
-        url: `https://api.adviceslip.com/advice`,
+        url: `https://api.adviceslip.com/advice`
 
     }).then((data) => {
         // console.log(data.slips[0].advice)
@@ -25,19 +27,43 @@ const getQuotes = () => {
         const h3forQuote = $('<h3>').text(data.slip.advice)
         // console.log(h3forQuote)
         $('.container').append(h3forQuote)
-
-
     })
 }
+
+const searchForQuotes = () => {
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: `https://api.adviceslip.com/advice/search/` + topic
+
+    }).then((data) => {
+        console.log(data.message.text)
+        // if(data.message.text === 'No advice slips found matching that search term.') {
+        //     console.log('broken')
+        })
+        const h3forTopic = $('<h3>').text(data.slips[0].advice)
+        // console.log(h3forQuote)
+        $('.container').append(h3forTopic)
+        
+   
+
+
+
 $(() => {
 
+    $('#quoteOftheDay').on('click', (event) => {
+        event.preventDefault()
+        
+        getQuotes()
+        
+    })
     $('#search').on('click', (event) => {
         event.preventDefault()
-        // getTopic()
-        getQuotes();
-
+        getTopic()
+        searchForQuotes()
     })
 })
+
 
 
 
